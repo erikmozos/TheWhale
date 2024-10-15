f94c07a31ca1e47cab9ece2a771745b9416772f7
erikmozos <mozoskp@gmail.com>
2024-10-15 21:09:11 +0200

    Homecasi listo

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+    // Selecciona los divs
+const procesDiv = document.querySelector('.proces');
+const historiaDiv = document.querySelector('.historia');
+
+// Añade evento de click para girar
+procesDiv.addEventListener('click', () => {
+    procesDiv.classList.toggle('active'); // Añade o elimina la clase 'active'
+});
+
+historiaDiv.addEventListener('click', () => {
+    historiaDiv.classList.toggle('active');
+});
+
+
+// Image Slider
+const slider = document.querySelector('.slider');
+const images = slider.querySelectorAll('img');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+function updateSlider() {
+    const offset = -currentIndex * 100; // Move slider based on current index
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1; // Loop to last image
+    updateSlider();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0; // Loop to first image
+    updateSlider();
+});
